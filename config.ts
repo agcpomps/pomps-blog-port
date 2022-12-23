2ec8714d8fe80d74946143176767fe2a027ad3e9
agcpomps <agcpomps@gmail.com>
2022-12-23 14:46:26 +0100

    adding header and other components

---
+++ config.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "Agc pomps personal website and portfolio";
+export const SITE_DESCRIPTION =
+  "A Blog site about programming and tech in general with a portfolio to showcase my work";
