bcf21d8642b2e6a6bf8d30d8853e1fd7766c94ad
agcpomps <agcpomps@gmail.com>
2023-01-27 12:04:48 +0100

    fixing tailwnd css bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://agcpomps.github.io",
-  integrations: [tailwind()]
+  integrations: [
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+  ],
 });
